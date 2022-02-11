c3595bb0ee249b091d208dd7af58cd13648e01aa
subham-proj <3133ss32@gmail.com>
2022-02-11 16:49:43 +0530

    deployment bug fix

--- backend/index.js
+++ backend/index.js
@@ -22,13 +22,12 @@ app.use("/api/users", userRoutes);
 
 // deployment conditions
 const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "./frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
+    res.sendFile(path.join(__dirname, "./frontend/build/index.html"))
   );
 } else {
   app.get("/", (req, res) => {
